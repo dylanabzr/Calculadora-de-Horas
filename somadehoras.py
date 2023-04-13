53247e2c9c9b4ff093a6d00756517f764b94956b
dylan <128552152+dylanabzr@users.noreply.github.com>
2023-04-13 20:35:01 -0300

    Update somadehoras.py

--- somadehoras.py
+++ somadehoras.py
@@ -13,31 +13,37 @@ def hora_lista(horario):
         return horario
 
 
-hora = hora_lista(input("Digite uma hora válida: "))
-hora2 = hora_lista(input("Digite outra hora válida para ser somada com a anterior: "))
-if hora is not None and hora2 is not None:
-    hora3 = [0, 0]
-    hora3[0] = hora[0] + hora2[0]
-    hora3[1] = hora[1] + hora2[1]
-    if hora3[0] > 23:
-        while hora3[0] > 23 or hora3[1] > 59:
-            if hora3[0] > 24:
-                hora3[0] = hora3[0] - 23
-                if hora3[1] > 59:
-                    hora3[1] = hora3[1] - 60
-                    hora3[0] = hora3[0] + 1
-            else:
+def soma_horas(hora, hora2):
+    hora = hora_lista(hora)
+    hora2 = hora_lista(hora2)
+    if hora is not None and hora2 is not None:
+        hora3 = [0, 0]
+        hora3[0] = hora[0] + hora2[0]
+        hora3[1] = hora[1] + hora2[1]
+        if hora3[0] > 23:
+            while hora3[0] > 23 or hora3[1] > 59:
+                if hora3[0] > 24:
+                    hora3[0] = hora3[0] - 23
+                    if hora3[1] > 59:
+                        hora3[1] = hora3[1] - 60
+                        hora3[0] = hora3[0] + 1
+                else:
+                    hora3[0] = 0
+                    if hora3[1] > 59:
+                        hora3[1] = hora3[1] - 60
+                        hora3[0] = hora3[0] + 1
+        elif hora3[0] <= 23 and hora3[1] > 59:
+            hora3[1] = hora3[1] - 60
+            hora3[0] = hora3[0] + 1
+            if hora3[0] == 24:
                 hora3[0] = 0
-                if hora3[1] > 59:
-                    hora3[1] = hora3[1] - 60
-                    hora3[0] = hora3[0] + 1
-    elif hora3[0] <= 23 and hora3[1] > 59:
-        hora3[1] = hora3[1] - 60
-        hora3[0] = hora3[0] + 1
-        if hora3[0] == 24:
-            hora3[0] = 0
-    hora3 = map(str, hora3)
-    hora3 = ":".join(hora3)
-    print(f"A hora final é {hora3}")
-else:
-    print("Horário inválido")
+        hora3 = map(str, hora3)
+        hora3 = ":".join(hora3)
+        print(f"A hora final é {hora3}")
+    else:
+        print("Horário inválido")
+
+
+a = input("Digite uma hora válida: ")
+b = input("Digite outra hora válida para ser somada com a anterior: ")
+soma_horas(a, b)

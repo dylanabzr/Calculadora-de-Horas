c985047033e507ea8d6f5be6f08f82da2b928add
Levi (dylan) <128552152+dylanabzr@users.noreply.github.com>
2023-04-15 16:56:17 -0300

    Criação de uma classe
    Primeira mudança: Substitui as duas funções anteriores por uma unica classe que soma as horas e que tem as duas funções inclusas nela.
    Motivo: Melhora a organização do código e permite importar a classe para outro código
    Segunda mudança: Agora caso um dos numeros (horas ou minutos) tenha apenas um dígito e a classe retorna o número com um 0 antes.
    Motivo: Quando acontecia uma soma por exemplo 12:20 + 13:46 as funções retornavam 3:6 sendo o correto 3:06 ou 03:06. Agora a classe retorna 03:06 nesse caso.

--- somadehoras.py
+++ somadehoras.py
@@ -1,49 +1,70 @@
-def hora_lista(horario): # Função que transforma uma string de hora no formato "XX:XX" em uma lista no formato [XX, XX]
-    horario = horario.replace(":", " ")
-    horario = horario.split()
-    horario_int = []
-    for i in horario:
-        horario_int.append(int(i))
-    horario = horario_int
-    if horario[1] > 59 or horario[1] < 0:
-        return None
-    elif horario[0] > 23 or horario[0] < 0:
-        return None
-    else:
-        return horario
+class SomaHoras:
+    def __init__(self, hora1, hora2):
+        """Função que recebe duas horas diferentes válidas e as soma"""
+        def hora_lista(horario):
+            """Função que transforma uma string de hora no formato "XX:XX" em uma lista no formato [XX, XX]"""
+            horario = horario.replace(":", " ")
+            horario = horario.split()
+            horario_int = []
+            for i in horario:
+                horario_int.append(int(i))
+            horario = horario_int
+            if horario[1] > 59 or horario[1] < 0:
+                return None
+            elif horario[0] > 23 or horario[0] < 0:
+                return None
+            else:
+                return horario
 
-
-def soma_horas(hora, hora2):  # Função que recebe duas horas diferentes válidas e as soma 
-    hora = hora_lista(hora)
-    hora2 = hora_lista(hora2)
-    if hora is not None and hora2 is not None:
-        hora3 = [0, 0]
-        hora3[0] = hora[0] + hora2[0]
-        hora3[1] = hora[1] + hora2[1]
-        if hora3[0] > 23:
-            while hora3[0] > 23 or hora3[1] > 59:
-                if hora3[0] > 24:
-                    hora3[0] = hora3[0] - 23
-                    if hora3[1] > 59:
-                        hora3[1] = hora3[1] - 60
-                        hora3[0] = hora3[0] + 1
-                else:
+        hora1 = hora_lista(hora1)
+        hora2 = hora_lista(hora2)
+        if hora1 is not None and hora2 is not None:
+            hora3 = [0, 0]
+            hora3[0] = hora1[0] + hora2[0]
+            hora3[1] = hora1[1] + hora2[1]
+            if hora3[0] > 23:
+                while hora3[0] > 23 or hora3[1] > 59:
+                    if hora3[0] > 24:
+                        hora3[0] = hora3[0] - 23
+                        if hora3[1] > 59:
+                            hora3[1] = hora3[1] - 60
+                            hora3[0] = hora3[0] + 1
+                    else:
+                        hora3[0] = 0
+                        if hora3[1] > 59:
+                            hora3[1] = hora3[1] - 60
+                            hora3[0] = hora3[0] + 1
+            elif hora3[0] <= 23 and hora3[1] > 59:
+                hora3[1] = hora3[1] - 60
+                hora3[0] = hora3[0] + 1
+                if hora3[0] == 24:
                     hora3[0] = 0
-                    if hora3[1] > 59:
-                        hora3[1] = hora3[1] - 60
-                        hora3[0] = hora3[0] + 1
-        elif hora3[0] <= 23 and hora3[1] > 59:
-            hora3[1] = hora3[1] - 60
-            hora3[0] = hora3[0] + 1
-            if hora3[0] == 24:
-                hora3[0] = 0
-        hora3 = map(str, hora3)
-        hora3 = ":".join(hora3)
-        print(f"A hora final é {hora3}")
-    else:
-        print("Horário inválido")
+            pass
+            if hora3[0] > 9 and hora3[1] > 9:
+                hora3 = map(str, hora3)
+                hora3 = ":".join(hora3)
+            elif hora3[0] < 9 < hora3[1]:
+                hora3[0] = str(hora3[0])
+                hora3[0] = "0" + hora3[0]
+                hora3 = map(str, hora3)
+                hora3 = ":".join(hora3)
+            elif hora3[1] < 9 < hora3[0]:
+                hora3[1] = str(hora3[1])
+                hora3[1] = "0" + hora3[1]
+                hora3 = map(str, hora3)
+                hora3 = ":".join(hora3)
+            elif hora3[1] < 9 and hora3[0] < 9:
+                hora3[0] = str(hora3[0])
+                hora3[0] = "0" + hora3[0]
+                hora3[1] = str(hora3[1])
+                hora3[1] = "0" + hora3[1]
+                hora3 = map(str, hora3)
+                hora3 = ":".join(hora3)
+            print(f"A hora final é {hora3}")
+        else:
+            print("Horário inválido")
 
 
 a = input("Digite uma hora válida: ")
 b = input("Digite outra hora válida para ser somada com a anterior: ")
-soma_horas(a, b)
+SomaHoras(a, b)
